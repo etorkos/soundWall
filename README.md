71758f81e35dc0d085181eeef55e4c019fdd0234
Evan Torkos <etorkos@gmail.com>
2015-05-05 19:15:05 -0400

    files ready to play with MPG321 instead of play. added sample sounds

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 Sound Wall, the Raspberry Pi - Arduino sound sensor which only creates ambient noise when it hears loud noises outside
+
+Software steps: 
+1: setup git
+2a: download serialPy
+2b: configure your usb bluetooth handshake with module
+3: download MPG321
+
+sudo apt-get -y install mpg321
+
+4: configure sound out of speakers
+
+in terminal run:
+lsmod | grep snd_bcm2835
+
+*if file is not found run
+sudo modprobe snd_bcm2835
+
+then force it to always load with
+cd /etc
+sudo nano modules
+
+# /etc/modules: kernel modules to load at boot time.
+# This file contains the names of kernel modules that should be
+# loaded at boot time, one per line. Lines beginning with "#" are
+# ignored. Parameters can be specified after the module name.
+ 
+snd-bcm2835
+
+amixer cset numid=3 1
+
+*credit to raspberrypi-spy.co.uk
--- audio_driver.py
+++ audio_driver.py
@@ -10,24 +10,24 @@ state= 'true'
 count = 0
 prevMaxNoise = 0
 myList = linkedList()
-#def loop (state):
-	#white = subprocess.Popen(['play', '-n', 'synth', 'whitenoise'], stdin=subprocess.PIPE);
+def loop (state):
+	brown = subprocess.Popen(['mpg321', '/samples/brown.mp3', '-g', '25']);
+	sleep(1)
+	subprocess.Popen.terminate()
+	brown = subprocess.Popen(['mpg321', '/samples/brown.mp3', '-g', '25']);
 	#sleep(.5)
-	#subprocess.Popen.terminate()
-	#white = subprocess.Popen(['play', '-n', 'synth', 'whitenoise']);
-	#sleep(.5)
-	#white.terminate()
+	brown.terminate()
 
 #stay at volume for 5 seconds then floor it
 
 
-def loop():
-	global prevMaxNoise
-	while state == 'true':
-		print("Checking bluetooth signal")
-		line = int( bluetoothSerial.readline())
-		myList.cycle(line)
-		volume = myList.frontNodeValue()
-		print "Current Volume %v" % volume
+# def loop():
+# 	global prevMaxNoise
+# 	while state == 'true':
+# 		print("Checking bluetooth signal")
+# 		line = int( bluetoothSerial.readline())
+# 		myList.cycle(line)
+# 		volume = myList.frontNodeValue()
+# 		print "Current Volume %v" % volume
 	
-loop()
+loop(state)
