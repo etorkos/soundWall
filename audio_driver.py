6ac26833811a22dfdaf2b152a0b14f53b53f5025
Evan Torkos <etorkos@gmail.com>
2015-05-05 19:43:52 -0400

    modified driver

--- audio_driver.py
+++ audio_driver.py
@@ -9,25 +9,30 @@ state= 'true'
 
 count = 0
 prevMaxNoise = 0
+sound = None
 myList = linkedList()
-def loop (state):
-	brown = subprocess.Popen(['mpg321', '/samples/brown.mp3', '-g', '25']);
-	sleep(1)
-	subprocess.Popen.terminate()
-	brown = subprocess.Popen(['mpg321', '/samples/brown.mp3', '-g', '25']);
-	#sleep(.5)
-	brown.terminate()
+# def loop (state):
+# 	brown = subprocess.Popen(['mpg321', 'samples/brown.mp3', '-g', '25']);
+# 	sleep(1)
+# 	brown.terminate()
+# 	brown = subprocess.Popen(['mpg321', 'samples/brown.mp3', '-g', '25']);
+# 	sleep(5)
+# 	brown.terminate()
 
 #stay at volume for 5 seconds then floor it
 
 
-# def loop():
-# 	global prevMaxNoise
-# 	while state == 'true':
-# 		print("Checking bluetooth signal")
-# 		line = int( bluetoothSerial.readline())
-# 		myList.cycle(line)
-# 		volume = myList.frontNodeValue()
-# 		print "Current Volume %v" % volume
+def loop():
+	global prevMaxNoise
+	while state == 'true':
+		print("Checking bluetooth signal")
+		line = int( bluetoothSerial.readline())
+		myList.cycle(line)
+		volume = myList.frontNodeValue()
+		print "Current Volume %v" % volume
+		command = "mpg321 samples/brown.mp3 -g %s" % (volume*6);
+		if sound is not None:
+			sound.terminate()
+		sound = subprocess.Popen(command);
 	
-loop(state)
+loop()
